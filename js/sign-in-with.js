2ae3291b5a8bed34f7868c8d339af0767060f533
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2022-12-29 12:13:28 +0100

    updated / simplified demo

--- js/sign-in-with.js
+++ js/sign-in-with.js
@@ -1,63 +1,41 @@
-window.passwordless = {
-  apiUrl: "https://api.passwordless.id",
-  //apiUrl: "http://localhost:8787",
-  init: async function() {
-    const args = new URLSearchParams({
-      scope: 'openid avatar',
-    })
-    // The API call to fetch the user
-    const res = await fetch(`${this.apiUrl}/openid/id_token?${args}`, {
-      mode: 'cors',
-      credentials: 'include'
-    })
+import passwordless from 'https://unpkg.com/@passwordless-id/connect'
 
-    if (res.ok) {
-      // Display it
-      const json = await res.json()
-      console.log(json)
-      // Please note that the JWT signature is not verified in this example
-      // The profile is simply extracted
-      const payload = json.id_token.split('.')[1]
-      const userinfo = JSON.parse(atob(payload.replaceAll('-', '+').replaceAll('_', '/')))
-      console.log(userinfo)
-      
-      showUser(userinfo)
-    
-    } else if (res.status === 401) {
-      // User must first sign in (or create account)
-      // It will be redirected back here once done
-      document.getElementById('login').className = 'visible'
-    } else if (res.status === 403) {
-      // User did not grant enough permissions (scope) 
-      document.getElementById('login').className = 'visible'
-    }
-    else {
-      console.warn(res)
-      alert(`Unexpected Error: ${res.status} ${await res.text()}`)
-    }
-  },
-  auth: async function() {
-    const args = new URLSearchParams({
-      response_type: 'none id_token',
-      client_id: window.location.origin,
-      scope: 'openid avatar',
-      redirect_uri: window.location.href,
-    })
-    window.location.assign(`${this.apiUrl}/openid/authorize?${args}`)
-  },
-  logout: function() {
-    const args = new URLSearchParams({
-      redirect_uri: window.location.href,
-    })
-    window.location.assign(`${this.apiUrl}/openid/logout?${args}`)
+const scope = 'openid avatar email'
+
+window.onClickSignIn = () => {
+  passwordless.auth({ scope })
+}
+
+window.onClickSignOut = async () => {
+  passwordless.logout()
+}
+
+async function init() {
+  const user = await passwordless.id({ scope })
+  if (user.signedIn && user.scopeGranted) {
+    console.log(user)
+    showUser(user)
+  }
+  else {
+    showSignIn()
   }
 }
 
-function showUser(userinfo) {
-  document.querySelector('#userinfo img').src = userinfo.picture
-  document.querySelector('#userinfo h3').textContent = userinfo.nickname ?? 'Error: username missing?!'
-  document.querySelector('#userinfo pre').textContent = JSON.stringify(userinfo, null, 2)
-  document.getElementById('userinfo').className = 'visible'
+init()
+
+function showUser(user) {
+  document.querySelector('#profile img').src = user.profile.picture
+  document.querySelector('#profile h1').textContent = user.profile.nickname
+  document.querySelector('#output code').textContent = JSON.stringify(user, null, ' ')
+
+  document.getElementById('spinner').hidden = true
+  document.getElementById('profile').hidden = false
+  document.getElementById('output').hidden = false
+  document.getElementById('sign-out').hidden = false
+
 }
 
-window.passwordless.init()
+function showSignIn() {
+  document.getElementById('spinner').hidden = true
+  document.getElementById('sign-in').hidden = false
+}
